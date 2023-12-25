0b16e9cc1d80f3e83b6c1fdb25794e25e71102f4
refleks1a <fhbxdfh.dhncdtb@gmail.com>
2023-12-25 16:38:27 +0400

    chare: changing frontend to the one in tutorial

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,9 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import App from "./App";
-import { store } from "./app/store";
+import store from "./store";
 import "./index.css";
-import * as serviceWorker from "./serviceWorker";
+import reportWebVitals from "./reportWebVitals"
 
 
 ReactDOM.render(
@@ -19,4 +19,4 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+reportWebVitals();
